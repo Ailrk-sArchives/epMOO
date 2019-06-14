f7d76c2269a51a4903ef93b2265e1556952310a7
Jimmy <jimmy123good@hotmail.com>
2019-06-13 19:29:55 -0700

    new

--- test/kur.py
+++ test/kur.py
@@ -8,20 +8,20 @@ import math
 
 def f1(x):
     s = 0
-    for i in range(len(x)-1):
-        s += -10*math.exp(-0.2*math.sqrt(x[i]**2 + x[i+1]**2))
+    for i in range(len(x) - 1):
+        s += -10 * math.exp(-0.2 * math.sqrt(x[i]**2 + x[i + 1] ** 2))
     return s
 
 
 def f2(x):
     s = 0
     for i in range(len(x)):
-        s += abs(x[i])**0.8 + 5*math.sin(x[i]**3)
+        s += abs(x[i])**0.8 + 5 * math.sin(x[i]**3)
     return s
 
 
 problem = Problem(num_of_variables=3, objectives=[f1, f2], variables_range=[(-5, 5)], same_range=True, expand=False)
-evo = Evolution(problem, mutation_param=20, num_of_generations=40, num_of_individuals=100, concurrency=True)
+evo = Evolution(problem, mutation_param=20, num_of_generations=40, num_of_individuals=500, concurrency=True, max_proc=20)
 func = [i.objectives for i in evo.evolve()]
 
 function1 = [i[0] for i in func]
